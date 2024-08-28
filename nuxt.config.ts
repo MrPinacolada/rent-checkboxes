bb22eebee2918f6a367366a9d564fe9375b54711
MrPinacolada <vasiasko112@gmail.com>
2024-08-28 16:10:37 +0400

    update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   app: {
-    baseURL: '/rent-chackboxes/'
+    baseURL: '/rent-checkboxes/',
+    buildAssetsDir: 'assets'
   }
 })
